2bc6ef172307e81bd248dddacbcdac5a5311f6ea
ikraammel <ikramelhoul@gmail.com>
2026-04-28 20:48:59 +0100

    suppression de dotenv pour le déploiement backend

--- src/main/java/com/smart_skin/smart_skin_app_backend/SmartSkinAppBackendApplication.java
+++ src/main/java/com/smart_skin/smart_skin_app_backend/SmartSkinAppBackendApplication.java
@@ -1,6 +1,5 @@
 package com.smart_skin.smart_skin_app_backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
