65be17acde2e733094e2746c168ba1166769ee3c
ikraammel <ikramelhoul@gmail.com>
2026-04-28 20:45:41 +0100

    suppression de dotenv pour le déploiement backend

--- src/main/java/com/smart_skin/smart_skin_app_backend/SmartSkinAppBackendApplication.java
+++ src/main/java/com/smart_skin/smart_skin_app_backend/SmartSkinAppBackendApplication.java
@@ -10,20 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SmartSkinAppBackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-        System.setProperty("RESEND_API_KEY", dotenv.get("RESEND_API_KEY"));
-
-        System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
-        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
-        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
-
-        System.setProperty("GROQ_API_KEY", dotenv.get("GROQ_API_KEY"));
-
         SpringApplication.run(SmartSkinAppBackendApplication.class, args);
     }
 
